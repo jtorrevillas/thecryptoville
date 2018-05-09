d09a3d9419a3f17b4a9a1e974e9dfb9ebb6b58a5
John Philip Limpahan <jlimpahan@ad.acquireap>
2018-05-09 12:17:59 +0800

    Added Tabbed Events

--- wp-content/plugins/cryptoville-events/cryptoville-events.php
+++ wp-content/plugins/cryptoville-events/cryptoville-events.php
@@ -8,30 +8,190 @@ add_action( 'init', 'custom_post_types_events');
 add_action( 'add_meta_boxes', 'add_events_meta_box' ); //ADD CUSTOM META BOX --EVENTS DETAILS
 add_action( 'save_post', 'events_meta_box_save' );	// SAVE CUSTOM META BOX DATA --EVENTS DETAILS
 add_action( 'wp_enqueue_scripts', 'wpse_load_plugin' );
-add_shortcode( 'cv_events_nav', 'create_events_filter_nav');
-
+add_shortcode( 'cv_events_content', 'cryptoville_events_load_contents');
+//add_shortcode( 'cv_events_nav', 'create_events_filter_nav');
 
 function wpse_load_plugin() {
     $plugin_url = plugin_dir_url( __FILE__ );
-    wp_enqueue_script('script', $plugin_url.'js/cryptoville_events_script.js', array('jquery'), 1, true);
-    wp_enqueue_style( 'style', $plugin_url . 'css/cryptoville_events_style.css' );
+
+    if(is_page('events')) :
+	    wp_register_script( 'jQueryUI', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', null, null, true );
+		wp_enqueue_script('jQueryUI');
+	    wp_enqueue_script('script', $plugin_url.'js/cryptoville_events_script.js', array('jquery'), 1, true);
+	    wp_enqueue_style( 'style', $plugin_url . 'css/cryptoville_events_style.css' );
+	endif;
     
 }
 
-function cryptoville_events_load_contents(){
-	global $post;
 
+function cryptoville_events_load_contents(){
+	
 	if(is_page('events')) :
-		print_r($post);
+		$params = array(
+	        'post_type'   => 'events',
+	        'tax_query' => array(
+				        array(
+				            'taxonomy' => 'etype_tax',
+				            'field' => 'slug',
+				            'terms' => array('featured_events','bitcoin_events','financial_events','technology_events')
+				        )
+				    )
+	    );
+		
+	    $events = new WP_Query($params);
+
+	    if( $events->have_posts() ) :
+	$all = '';
+	$btc = '';
+	$fin = '';
+	$tech = '';
+	$tempo = '';
+	            while( $events->have_posts()) :
+	                $events->the_post();
+	                
+	          		// $tags = $events->get_the_tags( get_the_ID() );
+
+					$eurl = (filter_var(get_post_meta(get_the_ID(),'event_url_mb',true),FILTER_VALIDATE_URL) 
+							 ? get_post_meta(get_the_ID(),'event_url_mb',true) : 'You entered an invalid URL!') ;
+		
+					$from = get_post_meta(get_the_ID(),'from_date_mb',true); 
+				    $to = get_post_meta(get_the_ID(),'to_date_mb',true);
+					$add = get_post_meta(get_the_ID(),'event_address_mb',true);
+					
+					$time = concat_date($from,$to);
+					
+
+					if(has_term('bitcoin_events','etype_tax')){
+						$btc.='<div style="margin-bottom:10px;margin-top:15px;"><a href="'.$eurl.'"><h4>'.get_the_title().'</h4></a></div>
+						<div style="display: inline-block; color: black; font-weight:600;">'.$time.'</div> <div style="display: inline-block;font-weight: 800;">|</div> <div style="display: inline-block; font-weight:600;">'.$add.'</div>
+						<div style="width: 100%; padding-top:15px;">
+						
+								'.get_the_content().'
+						
+						</div>
+						<hr>';
+					}else if(has_term('financial_events','etype_tax')){
+						$fin = '<div style="margin-bottom:10px;margin-top:15px;"><a href="'.$eurl.'"><h4>'.get_the_title().'</h4></a></div>
+						<div style="display: inline-block; color: black; font-weight:600;">'.$time.'</div> <div style="display: inline-block;font-weight: 800;">|</div> <div style="display: inline-block; font-weight:600;">'.$add.'</div>
+						<div style="width: 100%; padding-top:15px;">
+						
+								'.get_the_content().'
+						
+						</div>
+						<hr>';
+					}else if(has_term('technology_events','etype_tax')){
+						$tech = '<div style="margin-bottom:10px;margin-top:15px;"><a href="'.$eurl.'"><h4>'.get_the_title().'</h4></a></div>
+						<div style="display: inline-block; color: black; font-weight:600;">'.$time.'</div> <div style="display: inline-block;font-weight: 800;">|</div> <div style="display: inline-block; font-weight:600;">'.$add.'</div>
+						<div style="width: 100%; padding-top:15px;">
+						
+								'.get_the_content().'
+						
+						</div>
+						<hr>';
+					}
+
+					$all.='
+						<div style="margin-bottom:10px;margin-top:15px;"><a href="'.$eurl.'"><h4>'.get_the_title().'</h4></a></div>
+						<div style="display: inline-block; color: black; font-weight:600;">'.$time.'</div> <div style="display: inline-block;font-weight: 800;">|</div> <div style="display: inline-block; font-weight:600;">'.$add.'</div>
+						<div style="width: 100%; padding-top:15px;">
+						
+								'.get_the_content().'
+						
+						</div>
+						<hr>';
+
+	                $tempo .= '
+					<div class="col-lg-12" style="background-color: white;padding-top:10px;box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);margin-top: 15px;">
+						<div class="col-lg-4" >
+							<img class="vc_img-placeholder vc_single_image-img" src="'.get_the_post_thumbnail_url().'" />
+						</div>
+						<div class="col-lg-8" style="display:inline-block;">
+							<div><a href="'.$eurl.'"><h4>'.get_the_title().'</h4></a></div>
+							<div style="display: inline-block; color: black; font-weight:600;margin-top:10px;">'.$time.'</div> 
+								<div style="display: inline-block;font-weight: 900;margin-top:10px;">|</div> 
+								<div style="color: #a1a1a1; display: inline-block; font-weight:600;margin-top:10px;">'.$add.'</div>
+							<div style="width: 100%; padding-top:15px;margin-top:10px;">
+
+									'.get_the_content().'
+
+							</div>
+						</div>
+					</div><hr>
+					'; ?>
+
+	            <?php
+	            endwhile;
+	            wp_reset_postdata();
+
+
+
+	    endif;
+
+	     $tempo.='
+<div class="col-lg-12" style="margin:20px 0 0 0;padding:0;">
+	<div class="col-lg-8" style="padding:0;">
+		<div id="tabs">
+		  <ul>
+		    <li><a href="#tabs-1">All</a></li>
+		    <li><a href="#tabs-2">Bitcoin Events</a></li>
+		    <li><a href="#tabs-3">Financial Events</a></li>
+		    <li><a href="#tabs-4">Technology Events</a></li>
+		  </ul>
+		  <div id="tabs-1">
+		    '.$all.'
+		  </div>
+		  <div id="tabs-2">
+		    '.$btc.'
+		  </div>
+		  <div id="tabs-3">
+		    '.$fin.'
+		  </div>
+		  <div id="tabs-4">
+		   	'.$tech.'
+		  </div>
+		</div>
+	</div>
+	<div class="col-lg-4">
+	</div>
+</div>
+	     ';
+	    return $tempo;
 	endif;
+}
+
+function concat_date($from,$to){
+				$time = "";
+	
+				$f = explode("/",$from);
+				$t = explode("/",$to);
+	
+				$fd = date("F d, Y", mktime(0, 0, 0, $f[0], $f[1], $f[2]));
+				$td = date("F d, Y", mktime(0, 0, 0, $t[0], $t[1], $t[2]));
+
+				$fdd = explode(" ",$fd);
+				$tdd = explode(" ",$td);
+
+
+
+				if($fdd[0] == $tdd[0] && $fdd[1] == $tdd[1] && $fdd[2] == $tdd[2]){
+					$time = implode($fdd," ");
+				}else if ($fdd[0] == $tdd[0] && $fdd[2] == $tdd[2]){
+				$time = $fdd[0]." ".str_replace(",","",$fdd[1])."-".$tdd[1]." ".$fdd[2];
+
+				}else{
+					$time = implode($fdd," ")." - ".implode($tdd," ");
+				}
+				
+		return $time;
+	
 }
 
 function create_events_filter_nav(){
 	$content = '';
 	$content .='<div class="cv-container" id="cv-nav">';
-	$content .='	<div class="cv-item-filter"> <a href="#" > <i class="fa fa-flag-o" aria-hidden="true"></i> All Countries</a><div id="cv-dropdown-countries"></div></div>';
-	$content .='	<div class="cv-item-filter"> <a href="#" ><i class="fa fa-flag-o" aria-hidden="true"></i> All Cities</a><div id="cv-dropdown-cities"></div></div>';
-	$content .='	<div class="cv-item-filter"> <a href="#" ><i class="fa fa-flag-o" aria-hidden="true"></i> Any Month</a><div id="cv-dropdown-month"></div></div>';
+	$content .='	<div class="cv-item-filter"> <a href="#" id="cv-countries"> <i class="fa fa-flag-o" aria-hidden="true"></i> All Countries</a><div id="cv-dropdown-countries"></div></div>';
+	$content .='	<div class="cv-item-filter"> <a href="#" id="cv-cities" ><i class="fa fa-map-marker" aria-hidden="true"></i> All Cities</a><div id="cv-dropdown-cities"></div></div>';
+	$content .='	<div class="cv-item-filter"> <a href="#" id="cv-month" ><i class="fa fa-calendar" aria-hidden="true"></i> Any Month</a><div id="cv-dropdown-month"></div></div>';
 	$content .='	<div class="cv-item-filter">Holding an event?<a href="#">   Let us know</a></div>';
 	$content .='</div>';
 
--- wp-content/plugins/cryptoville-events/js/cryptoville_events_script.js
+++ wp-content/plugins/cryptoville-events/js/cryptoville_events_script.js
@@ -1,61 +1,90 @@
+
 (function($){
-
-			let div_countries = jQuery('#cv-dropdown-countries');
-			let div_cities = jQuery('#cv-dropdown-cities');
-			let a_countries = jQuery('#cv-countries');
-			let a_cities = jQuery('#cv-cities');
-			let a_month = jQuery('#cv-month');
-			let countries = '';
-			let cities = '';
-
-	let plugin = {
-			
-			loadContents : function(){
-
-				if(countries == '' && cities == ''){
-				
-					(jQuery).ajax({
-						url: "https://restcountries.eu/rest/v2/all",
-						type: "GET",
-						dataType: 'json',
-					}).done(function(data){
-								
-							
-							countries+='<ul>';
-							cities+='<ul>';
-								
-							(jQuery).each(data,function(k,v){
-
-								if(v.name.length != 0 || v.capital.length != 0)
-								countries+='<li><a href="#">'+v.name+'</a></li>';
-								cities+='<li><a href="#">'+v.capital+'</a></li>';
-							});
-							countries+='</ul>';
-							cities+='</ul>';
-
-							
-							div_countries.html(countries);
-							div_cities.html(cities);
-
-
-					});
-				}
-			}
-	}
-
-
-	plugin.loadContents();
-
-	
-	a_countries.on('click',function(){
-			div_countries.css('','block');
-	});
-
-
-
-
-
-
-
-
+	$('#tabs').tabs();
 })(jQuery);
+
+// (function($){
+
+// 			let div_countries = jQuery('#cv-dropdown-countries');
+// 			let div_cities = jQuery('#cv-dropdown-cities');
+
+// 			let a_countries = jQuery('#cv-countries');
+// 			let a_cities = jQuery('#cv-cities');
+// 			let a_month = jQuery('#cv-month');
+// 			let countries = '';
+// 			let cities = '';
+
+// 	let plugin = {
+			
+// 			loadContents : function(){
+
+// 				if(countries == '' && cities == ''){
+				
+// 					(jQuery).ajax({
+// 						url: "https://restcountries.eu/rest/v2/all",
+// 						type: "GET",
+// 						dataType: 'json',
+// 					}).done(function(data){
+								
+							
+// 							countries+='<ul id="countries" style="list-style-type:none; color:white;">';
+// 							cities+='<ul style="list-style-type:none; color:white;">';
+								
+// 							(jQuery).each(data,function(k,v){
+
+// 								if(v.name.length != 0 || v.capital.length != 0)
+// 								countries+='<li>'+v.name+'</li>';
+// 								cities+='<li>'+v.capital+'</li>';
+// 							});
+// 							countries+='</ul>';
+// 							cities+='</ul>';
+
+							
+// 							div_countries.html(countries);
+// 							div_cities.html(cities);
+
+// 					});
+// 				}
+// 			}
+// 	}
+
+
+// 	plugin.loadContents();
+
+// 	let isShowing1 = false;
+	
+// 	a_countries.on('click',function(e){
+// 		 e.preventDefault();
+// 		 if(!isShowing1){
+// 		 	div_countries.css('display','block');
+// 		 	isShowing1 = true;
+// 		 }else{
+// 		 	div_countries.css('display','none');
+// 		 	isShowing1 = false;
+// 		 }
+// 	});
+
+// 	 let isShowing2 = false;
+// 	a_cities.on('click',function(e){
+// 		e.preventDefault();
+// 		 if(!isShowing2){
+// 		 	div_cities.css('display','block');
+// 		 	isShowing2 = true;
+// 		 }else{
+// 		 	div_cities.css('display','none');
+// 		 	isShowing2 = false;
+// 		 }
+// 	});
+
+// 	$('ul[id*=countries] li').click(function(){
+// 		console.log($(this).text());
+// 	});
+
+
+
+
+
+
+
+
+// })(jQuery);
