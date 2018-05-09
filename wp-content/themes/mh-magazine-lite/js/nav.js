2df20d2d2b176426a4b7b54d315b17b1beb5c6d8
John Jeevon Ang <john.an@ibinex.tech>
2018-05-09 12:13:41 +0800

    nav fix

--- wp-content/themes/mh-magazine-lite/js/nav.js
+++ wp-content/themes/mh-magazine-lite/js/nav.js
@@ -1,1 +1,38 @@
-var active;function convertToSlug(e){return e.toLowerCase().replace(/[^\w ]+/g,"").replace(/ +/g,"-")}function attachClickListener(e,t){document.querySelectorAll("#"+e+" a").forEach(function(e){e.addEventListener("click",function(e){sessionStorage.setItem("activeNav",t)})})}function createNavDom(e){var t=e.childNodes,n=convertToSlug(e.childNodes[0].innerText),a=Array.prototype.indexOf.call(e.parentNode.children,e)+1;if(n!=active){var c=jQuery(".active");c.length&&c.removeClass("active");var i=document.getElementById(n);if(i)active=n,i.classList.add("active");else{var r=t[2];r.setAttribute("id",n),attachClickListener(n,a),document.getElementById(n).classList.add("active"),document.querySelector(".mh-dp-menu").appendChild(r)}}}setTimeout(function(){createNavDom(document.querySelector("#menu-header-menu>li:nth-child("+(sessionStorage.getItem("activeNav")||2)+")")),2!=sessionStorage.getItem("activeNav")&&sessionStorage.setItem("activeNav",2),document.querySelectorAll("#menu-header-menu>li:not(:first-child)").forEach(function(e){e.addEventListener("mouseenter",function(){createNavDom(this)}),e.addEventListener("click",function(e){e.preventDefault()})})},200);
+var active;
+
+function convertToSlug(e) {
+    return e.toLowerCase().replace(/[^\w ]+/g, "").replace(/ +/g, "-")
+}
+
+function attachClickListener(e, t) {
+    document.querySelectorAll("#" + e + " a").forEach(function(e) {
+        e.addEventListener("click", function(e) {
+            sessionStorage.setItem("activeNav", t)
+        })
+    })
+}
+
+function createNavDom(e) {
+    var t = e.childNodes,
+        n = convertToSlug(e.childNodes[0].innerText),
+        c = Array.prototype.indexOf.call(e.parentNode.children, e) + 1;
+    if (n != active && t.length > 1) {
+        var a = jQuery(".active");
+        a.length && a.removeClass("active");
+        var i = document.getElementById(n);
+        if (i) active = n, i.classList.add("active");
+        else {
+            var o = t[2];
+            o.setAttribute("id", n), attachClickListener(n, c), document.getElementById(n).classList.add("active"), document.querySelector(".mh-dp-menu").appendChild(o)
+        }
+    }
+}
+setTimeout(function() {
+    createNavDom(document.querySelector("#menu-header-menu>li:nth-child(" + (sessionStorage.getItem("activeNav") || 2) + ")")), 2 != sessionStorage.getItem("activeNav") && sessionStorage.setItem("activeNav", 2), document.querySelectorAll("#menu-header-menu>li:not(:first-child)").forEach(function(e) {
+        e.addEventListener("mouseenter", function() {
+            createNavDom(this)
+        }), e.addEventListener("click", function(e) {
+            this.childNodes.legnth > 1 && e.preventDefault()
+        })
+    })
+}, 200);
